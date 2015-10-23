2cb06b3af7b293f6ac8c1e1f7b566caa6e234ba6
Mario Esposito <mario.esposito@kywix.com>
2015-10-23 15:18:13 -0700

    added code for post on C# on OS X

---
+++ OSX/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace HelloWorld
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
