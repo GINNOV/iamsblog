ab779deb39f20d8f42816ac274dc84f93f3d8906
Mario Esposito <mario.esposito@kywix.com>
2015-11-25 17:54:44 -0800

    - Added support for tracking IR temperature - Formatted and further commented the code - Generalized code to support other sensors as well

--- SensorTag/logger.js
+++ SensorTag/logger.js
@@ -1,8 +1,5 @@
 /*
-	SensorTag Accelerometer example from I am Sensoria blog
-
-	This example uses Sandeep Mistry's sensortag library for node.js to
-	read data from a TI sensorTag.
+	SensorTag data logger from IAmSensoria.com blog
 	
 	Although the sensortag library functions are all asynchronous,
 	there is a sequence you need to follow in order to successfully
@@ -18,49 +15,82 @@
 	This example is heavily indebted to Sandeep's test for the library, but
 	achieves more or less the same thing without using the async library.
 	
-	created 18 Nov 2015
-	by Mario Esposito (forked from Tom Igoe)
+	Sandeep Mistry's sensortag library for node.js is leveraged to read data from a TI sensorTag.
+
+	created on 18 Nov 2015
+	by Mario Esposito (forked from Tom Igoe COMMIT-SHA 3bf2c26fb1e4e9b7162f76a786fb9a530e2f2939)
 */
 
 
-var SensorTag = require('sensortag');			// sensortag library
+var SensorTag = require('sensortag'); // sensortag library
 
 // listen for tags:
 SensorTag.discover(function(tag) {
+
 	// when you disconnect from a tag, exit the program:
+	//
 	tag.on('disconnect', function() {
-		console.log('disconnected!');
+		console.log('Device Disconnected!');
 		process.exit(0);
 	});
 
-	function connectAndSetUpMe() {				// attempt to connect to the tag
-     console.log('connectAndSetUp');
-     tag.connectAndSetUp(enableAccelMe);		// when you connect and device is setup, call enableAccelMe
-   }
+	//
+	// Connect and setup
+	//
+	function connectAndSetUpMe() { // attempt to connect to the tag
+		console.log('Connecting...');
+		tag.connectAndSetUp(enableSensors); // when you connect and device is setup, call enableAccelMe
+	}
 
-   function enableAccelMe() {					// attempt to enable the accelerometer
-     console.log('enableAccelerometer');
-     // when you enable the accelerometer, start accelerometer notifications:
-     tag.enableAccelerometer(notifyMe);
-     console.log('Epoch,X,Y,Z');
-   }
+	//
+	// This function enables the accelerometer stream
+	//
+	function enableSensors() { // attempt to enable the accelerometer
+		console.log('Enable Sensors (Acc, IR Temp)');
+		// when you enable the accelerometer, start accelerometer notifications:
+		tag.enableAccelerometer(notifyMe); // start the accelerometer listner
+		tag.enableIrTemperature(notifyMe); // start the IR temp sensor listner
+		console.log('Timestamp,X,Y,Z,Objtemp,AmbientTemp');
+	}
 
+	//
+	// Activate which service we want to be notified by
+	//
 	function notifyMe() {
-   	tag.notifyAccelerometer(listenForAcc);   	// start the accelerometer listener
-		tag.notifySimpleKey(listenForButton);	// start the button listener
-   }
+		tag.notifyAccelerometer(notificationManager); // setup call back for accelerometer
+		tag.notifyIrTemperature(notificationManager); // setup call back for IR temp
+		tag.notifySimpleKey(listenForButton); // setup call back for button/switches
+	}
 
-   // When you get an accelermeter change, print it out:
-	function listenForAcc() {
+	//
+	// When you get an accelermeter change, print it out:
+	//
+	function notificationManager() {
+		var at = 0;
+		var ot = 0;
+
+		tag.on('irTemperatureChange', function(objectTemp, ambientTemp) {
+			ot = objectTemp.toFixed(1);
+			at = ambientTemp.toFixed(1);
+		});
+
 		tag.on('accelerometerChange', function(x, y, z) {
-	     // console.log('\tx = %d G', x.toFixed(1));
-	     // console.log('\ty = %d G', y.toFixed(1));
-	     // console.log('\tz = %d G', z.toFixed(1));
-	     console.log('%s,%d,%d,%d', getDateTime(), x.toFixed(1), y.toFixed(1), z.toFixed(1));
-	   });
+			console.log('%s,%d,%d,%d,%d,%d', getDateTime(), x.toFixed(1), y.toFixed(1), z.toFixed(1), ot, at);
+		});
+	}
+
+	//
+	// When IR temperature new data comes in  (not used in this code, left only in case you want to track it independently)
+	//
+	function listenForTempReading() {
+		tag.on('irTemperatureChange', function(objectTemp, ambientTemp) {
+			console.log('\tObject Temp = %d deg. C', objectTemp.toFixed(1));
+			console.log('\tAmbient Temp = %d deg. C', ambientTemp.toFixed(1));
+		});
 	}
 
 	// when you get a button change, print it out:
+	//
 	function listenForButton() {
 		tag.on('simpleKeyChange', function(left, right) {
 			if (left) {
@@ -73,7 +103,7 @@ SensorTag.discover(function(tag) {
 			if (left && right) {
 				tag.disconnect();
 			}
-	   });
+		});
 	}
 
 	//
@@ -103,6 +133,7 @@ SensorTag.discover(function(tag) {
 		return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
 
 	}
+
 	// Now that you've defined all the functions, start the process:
 	connectAndSetUpMe();
 });
