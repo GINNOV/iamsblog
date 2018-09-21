96410f5667fd4a78ec738272c4848c6db38d245e
Dunno Much <13722925+GINNOV@users.noreply.github.com>
2018-09-20 18:52:08 -0700

    updated bash script with docker tools

--- 68K/toolchain.sh
+++ 68K/toolchain.sh
@@ -2,6 +2,43 @@
 
 BASE="$HOME/amigatools"
 
+# AMIGA TOOLCHAIN
+#
+
+#####################################################
+############# START DOCKER ##########################
+#####################################################
+
+function d_amigcc() {
+	# if [[ ! -f "$1"  && ( ! -d "$1" || ! -f "$1/$2" ) ]]; then
+	#     echo "$1 not found"
+	#     echo "$2 not found"
+	#     echo "Usage: $0 [executable folder] <amiga executable>"
+	#     return
+	# fi
+	
+	# $1 = folder
+	# $2 = source code to compile without ext
+	docker run -v $PWD/"$1":/opt/folder -it sebastianbergmann/m68k-amigaos-bebbo m68k-amigaos-gcc opt/folder/"$2".c -o opt/folder/"$2" -noixemul
+}
+
+function d_amirun() {
+	# $1 = folder
+	# $2 = exectable to run
+	docker run -v $PWD/"$1":/opt/folder sebastianbergmann/amitools:latest vamos -C 68020 opt/folder/"$2"
+}
+
+function d_amivasm() {
+	# $1 = folder
+	# $2 = source code to compile without ext
+	# $3 = exectable name
+	docker run -v $PWD/"$1":/opt/folder sebastianbergmann/m68k-amigaos-bebbo vasm -Fhunkexe -o opt/folder/"$3" opt/folder/"$2".s
+}
+
+###################################################
+############# END DOCKER ##########################
+###################################################
+
 # Build using vasm and NDK
 #
 function amiBuildN() {
